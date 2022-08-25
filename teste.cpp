96988d4faef6c349885242cc4ee9413f91560070
Thiago Solfo <61445366+ThiagoSolfo@users.noreply.github.com>
2022-08-25 08:30:05 -0300

    Comentários adicionais & codificação Windows 1252

--- teste.cpp
+++ teste.cpp
@@ -1,90 +1,94 @@
 /*
-
 Define uma cartela ordenada do bingo.
-Ãšltima AlteraÃ§Ã£o em 23/08/2022 por Homero.
+Última Alteração em 23/08/2022 por Homero.
 
-Faltante: 
-        - Criar 5 cartelas, cada uma com seu Ã­ndice (Matriz com 3 dimensÃµes);
-        - Criar a MecÃ¢nica de Bingo em si;
-        - Realizar a associaÃ§Ã£o do nÃºmero de saÃ­da com cada cartela;
-        - VerificaÃ§Ã£o de ganhador do Bingo;
-        - Menu de InteraÃ§Ã£o do UsuÃ¡rio;
-        - Interface com as cartelas atualizando a medida que os nÃºmeros vÃ£o saindo;
-        - Loop com o jogo completo no main.
+Integrantes:
+  - Homero César de Campos
+  - Kevin Robert Elias
+  - Thiago Marinho Solfo
+  - Vinicius José
 
+Faltante:
+  - Criar 5 cartelas, cada uma com seu í­ndice (Matriz com 3 dimensões);
+  - Criar a Mecânica de Bingo em si;
+  - Realizar a associação do número de saí­da com cada cartela;
+  - Verificação de ganhador do Bingo;
+  - Menu de interação do usuário;
+  - Interface com as cartelas atualizando a medida que os números vão saindo;
+  - Loop com o jogo completo no main.
 */
 
-#include <iostream>
 #include <locale.h>
 #include <stdlib.h>
 #include <time.h>
+#include <iostream>
+#include "codefun.h"
 
 using namespace std;
 
 #define SIZE 5
 
-void inicializaLinha(int vet[SIZE]){
-    for (int i = 0; i<SIZE; i++)
-        vet[i] = 0;  
+void inicializaLinha(int vet[SIZE]) {
+    for (int i = 0; i < SIZE; i++)
+        vet[i] = 0;
 }
 
-bool SIS(int vet[SIZE], int valor, int tam){
-    for (int k = 0;k<tam;k++){
-        if(valor == vet[k])
+bool SIS(int vet[SIZE], int valor, int tam) {
+    for (int k = 0; k < tam; k++) {
+        if (valor == vet[k])
             return true;
-        
     }
     return false;
 }
 
-void sort(int vet[]) {              //bubble sort
+void sort(int vet[]) {  // bubble sort
     int i, j, cond, temp;
     cond = 1;
-    for (i=SIZE-1; (i >= 1) && (cond == 1); i--) {
+    for (i = SIZE - 1; (i >= 1) && (cond == 1); i--) {
         cond = 0;
-        for (j=0; j < i ;j++) {
-            if (vet[j+1] < vet[j]) {
+        for (j = 0; j < i; j++) {
+            if (vet[j + 1] < vet[j]) {
                 temp = vet[j];
-                vet[j] = vet[j+1];
-                vet[j+1] = temp;
+                vet[j] = vet[j + 1];
+                vet[j + 1] = temp;
                 cond = 1;
             }
         }
     }
 }
 
-void preencheLinha(int vet[SIZE],int linha){
+void preencheLinha(int vet[SIZE], int linha) {
     inicializaLinha(vet);
     int p;
-    for (int i = 0;i<SIZE;i++){
-        do{
-            p = rand()%15+(1+linha*15);
-        } while(SIS(vet,p,i));
+    for (int i = 0; i < SIZE; i++) {
+        do {
+            p = rand() % 15 + (1 + linha * 15);
+        } while (SIS(vet, p, i));
         vet[i] = p;
-    } 
-    sort(vet);   
+    }
+    sort(vet);
 }
 
-void preencheCartela(int vet[SIZE][SIZE]){
+void preencheCartela(int vet[SIZE][SIZE]) {
     int linha[SIZE];
-    for (int i = 0;i<SIZE;i++){
-        preencheLinha(linha,i);
-        for (int j = 0;j<SIZE;j++)
+    for (int i = 0; i < SIZE; i++) {
+        preencheLinha(linha, i);
+        for (int j = 0; j < SIZE; j++)
             vet[i][j] = linha[j];
-    }    
- }
+    }
+}
 
- void imprimeCartela(int vet[SIZE][SIZE]){
-    for (int i = 0;i<SIZE;i++){
+void imprimeCartela(int vet[SIZE][SIZE]) {
+    for (int i = 0; i < SIZE; i++) {
         cout << endl;
-        for (int j = 0;j<SIZE;j++){
+        for (int j = 0; j < SIZE; j++) {
             cout << vet[i][j] << " ";
         }
     }
- }
+}
 
-int main(){
-    setlocale(LC_ALL,"portuguese");
+int main() {
+    setlocale(LC_ALL, "Portuguese");
     srand(time(NULL));
     int Cartela1[SIZE][SIZE];
     preencheCartela(Cartela1);
